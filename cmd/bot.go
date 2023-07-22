5529fa9ca32cdaae26d363c9ff90bbbad8c9e53c
Ben Green <greencommaben@gmail.com>
2023-07-22 15:14:53 -0600

    multi-ship auto-miner

--- cmd/bot.go
+++ cmd/bot.go
@@ -6,6 +6,8 @@ package cmd
 import (
 	"fmt"
 	"math"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/bgreen/space-traders-go/stapi"
@@ -23,28 +25,55 @@ var botCmd = &cobra.Command{
 }
 
 var minerCmd = &cobra.Command{
-	Use:   "miner <ship>",
+	Use:   "miner [ship]",
 	Short: "An auto-mining bot",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		for {
-			mineUntilFull(args[0])
-			sellAllCargo(args[0])
+		var wg sync.WaitGroup
+		if len(args) == 1 {
+			wg.Add(1)
+			go mineBot(&wg, args[0])
+		} else if len(args) == 0 {
+			ships, err := client.GetMyShips()
+			if err != nil {
+				return
+			}
+			for _, ship := range ships {
+				if isShipMiner(ship) {
+					wg.Add(1)
+					go mineBot(&wg, ship.Symbol)
+				}
+			}
 		}
+		wg.Wait()
 	},
 }
 
-func mineUntilFull(shipSymbol string) {
+func mineBot(wg *sync.WaitGroup, shipSymbol string) {
+	for {
+		err := mineUntilFull(shipSymbol)
+		if err != nil {
+			break
+		}
+		err = sellAllCargo(shipSymbol)
+		if err != nil {
+			break
+		}
+	}
+	wg.Done()
+}
+
+func mineUntilFull(shipSymbol string) error {
 	ship, err := client.GetMyShip(shipSymbol)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return err
 	}
 
 	waypoints, err := client.GetSystemWaypoints(ship.Nav.SystemSymbol)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return err
 	}
 
 	// Get the full waypoint info
@@ -56,43 +85,50 @@ func mineUntilFull(shipSymbol string) {
 		}
 	}
 
-	fmt.Printf("Current location: %v (%v, %v)\n", wp.Symbol, wp.X, wp.Y)
-
 	closest := findClosestWaypoint(wp, waypoints, isWaypointMine)
 
-	fmt.Printf("Closest mine: %v (%v, %v) at %v units away\n", closest.Symbol, closest.X, closest.Y, waypointDistance(wp, closest))
-
-	navAndRefuel(ship, closest)
+	err = navAndRefuel(ship, closest)
+	if err != nil {
+		return err
+	}
 
 	// Check if there's a cooldown active
 	cd, _ := client.GetShipCooldown(ship.Symbol)
 	if cd.RemainingSeconds > 0 {
 		t := time.Until(*cd.Expiration)
-		fmt.Printf("Waiting %v for cooldown\n", t)
+		fmt.Printf("%v: Waiting %v for cooldown\n", shipSymbol, t)
 		time.Sleep(t)
-		fmt.Printf("Cooldown complete\n")
 	}
 
 	for {
 		r0, err := client.ExtractResources(shipSymbol)
 		if err != nil {
-			return
+			return err
 		}
 
-		fmt.Printf("Mined %v units of %v\n", r0.Extraction.Yield.Units, r0.Extraction.Yield.Symbol)
+		t := time.Until(*r0.Cooldown.Expiration)
 
-		fmt.Printf("Cargo at %v/%v\n", r0.Cargo.Units, r0.Cargo.Capacity)
+		fmt.Printf("%v: Mined %2v units of %16v; cargo at %2v/%2v; cooldown %v\n", shipSymbol,
+			r0.Extraction.Yield.Units, r0.Extraction.Yield.Symbol,
+			r0.Cargo.Units, r0.Cargo.Capacity, t)
 
 		if r0.Cargo.Units == r0.Cargo.Capacity {
 			break
 		}
 
-		t := time.Until(*r0.Cooldown.Expiration)
-		fmt.Printf("Waiting %v for cooldown\n", t)
 		time.Sleep(t)
-		fmt.Printf("Cooldown complete\n")
 	}
-	fmt.Println("Done mining")
+
+	return nil
+}
+
+func isShipMiner(s stapi.Ship) bool {
+	for _, v := range s.Mounts {
+		if strings.Contains(v.Symbol, "MINING_LASER") {
+			return true
+		}
+	}
+	return false
 }
 
 func isWaypointMine(w stapi.Waypoint) bool {
@@ -114,6 +150,13 @@ func waypointDistance(w1, w2 stapi.Waypoint) float64 {
 	return c
 }
 
+func routeDistance(r stapi.ShipNavRoute) float64 {
+	a := math.Abs(float64(r.Departure.X - r.Destination.X))
+	b := math.Abs(float64(r.Departure.Y - r.Destination.Y))
+	c := math.Sqrt(a*a + b*b)
+	return c
+}
+
 func findClosestWaypoint(current stapi.Waypoint, waypoints []stapi.Waypoint, filter func(stapi.Waypoint) bool) stapi.Waypoint {
 
 	var closest stapi.Waypoint
@@ -130,61 +173,81 @@ func findClosestWaypoint(current stapi.Waypoint, waypoints []stapi.Waypoint, fil
 	return closest
 }
 
-func navAndRefuel(ship stapi.Ship, dest stapi.Waypoint) {
+func navAndRefuel(ship stapi.Ship, dest stapi.Waypoint) error {
+
 	// If ship is docked, undock
 	if ship.Nav.Status == "DOCKED" {
 		r0, _ := client.OrbitShip(ship.Symbol)
-		fmt.Printf("Ship %v\n", r0.Nav.Status)
+		fmt.Printf("%v: Ship %v\n", ship.Symbol, r0.Nav.Status)
+	} else if ship.Nav.Status == "IN_TRANSIT" {
+		// Wait for arrival
+		t := time.Until(ship.Nav.Route.Arrival)
+		fmt.Printf("%v: In transit, waiting %v to arrive\n", ship.Symbol, t)
+		time.Sleep(t)
+
+		// Refresh ship data
+		ship, _ = client.GetMyShip(ship.Symbol)
 	}
 
 	// If ship isn't already there, go there
 	if ship.Nav.WaypointSymbol != dest.Symbol {
 
 		// Set thrusters to slow
-		if isLowFuel(ship.Fuel) && (ship.Nav.FlightMode != "DRIFT") {
+		if ship.Fuel.Current == 0 {
+			fmt.Printf("%v: No fuel to navigate, bailing\n", ship.Symbol)
+			return fmt.Errorf("no fuel")
+		} else if isLowFuel(ship.Fuel) && (ship.Nav.FlightMode != "DRIFT") {
 			r1, _ := client.PatchShipNav(ship.Symbol, "DRIFT")
-			fmt.Printf("Set flight mode to %v due to low fuel (%v/%v)\n", r1.FlightMode, ship.Fuel.Current, ship.Fuel.Capacity)
+			fmt.Printf("%v: Set flight mode to %v due to low fuel (%v/%v)\n", ship.Symbol, r1.FlightMode, ship.Fuel.Current, ship.Fuel.Capacity)
 		} else if ship.Nav.FlightMode != "CRUISE" {
 			r1, _ := client.PatchShipNav(ship.Symbol, "CRUISE")
-			fmt.Printf("Set flight mode to %v\n", r1.FlightMode)
+			fmt.Printf("%v: Set flight mode to %v\n", ship.Symbol, r1.FlightMode)
 		}
 
 		// Go to the destination
 		r2, err := client.NavigateShip(ship.Symbol, dest.Symbol)
 		if err != nil {
-			return
+			return err
 		}
-		fmt.Printf("Navigating to %v\n", r2.Nav.WaypointSymbol)
+		from := r2.Nav.Route.Departure
+		to := r2.Nav.Route.Destination
 
 		// Wait to get there
 		t := time.Until(r2.Nav.Route.Arrival)
-		fmt.Printf("Waiting %v to arrive\n", t)
+
+		fmt.Printf("%v: Navigating from %v (%v, %v) to %v (%v, %v), total %v units. Arriving in %v\n",
+			ship.Symbol,
+			from.Symbol, from.X, from.Y,
+			to.Symbol, to.X, to.Y,
+			routeDistance(r2.Nav.Route), t)
+
 		time.Sleep(t)
-		fmt.Println("Arrived")
 
 		// Refuel
 		if isWaypointMarket(dest) && isLowFuel(r2.Fuel) {
-			fmt.Printf("Refueling due to low fuel (%v/%v)\n", r2.Fuel.Current, r2.Fuel.Capacity)
+			fmt.Printf("%v: Refueling due to low fuel (%v/%v)\n", ship.Symbol, r2.Fuel.Current, r2.Fuel.Capacity)
 			client.DockShip(ship.Symbol)
 			client.RefuelShip(ship.Symbol)
 			client.OrbitShip(ship.Symbol)
 		}
 	}
+
+	return nil
 }
 
 func isLowFuel(fuel stapi.ShipFuel) bool {
 	return (fuel.Current == 0) || (float64(fuel.Current)/float64(fuel.Capacity) <= 0.5)
 }
 
-func sellAllCargo(shipSymbol string) {
+func sellAllCargo(shipSymbol string) error {
 	ship, err := client.GetMyShip(shipSymbol)
 	if err != nil {
-		return
+		return err
 	}
 
 	waypoints, err := client.GetSystemWaypoints(ship.Nav.SystemSymbol)
 	if err != nil {
-		return
+		return err
 	}
 
 	// Get the full waypoint info
@@ -196,13 +259,12 @@ func sellAllCargo(shipSymbol string) {
 		}
 	}
 
-	fmt.Printf("Current location: %v (%v, %v)\n", wp.Symbol, wp.X, wp.Y)
-
 	closest := findClosestWaypoint(wp, waypoints, isWaypointMarket)
 
-	fmt.Printf("Closest market: %v (%v, %v) at %v units away\n", closest.Symbol, closest.X, closest.Y, waypointDistance(wp, closest))
-
-	navAndRefuel(ship, closest)
+	err = navAndRefuel(ship, closest)
+	if err != nil {
+		return err
+	}
 
 	// Try to dock, ignore error if already docked
 	client.DockShip(ship.Symbol)
@@ -213,16 +275,17 @@ func sellAllCargo(shipSymbol string) {
 		if isMarketBuying(market, v.Symbol) {
 			r0, err := client.SellCargo(ship.Symbol, stapi.TradeSymbol(v.Symbol), v.Units)
 			if err != nil {
-				fmt.Printf("Couldn't sell %v units of %v\n", v.Units, v.Symbol)
+				fmt.Printf("%v: Couldn't sell %v units of %v\n", ship.Symbol, v.Units, v.Symbol)
 			} else {
 				t := r0.Transaction
-				fmt.Printf("Sold %v units of %v at %v for a total of %v\n", t.Units, t.TradeSymbol, t.PricePerUnit, t.TotalPrice)
+				fmt.Printf("%v: Sold %2v units of %16v at %2v for a total of %3v\n", ship.Symbol, t.Units, t.TradeSymbol, t.PricePerUnit, t.TotalPrice)
 			}
 		} else {
-			fmt.Printf("Market does not buy %v\n", v.Symbol)
+			fmt.Printf("%v: Market does not buy %v\n", ship.Symbol, v.Symbol)
 		}
 	}
-	fmt.Println("Done selling")
+
+	return nil
 }
 
 func isMarketBuying(market stapi.Market, trade string) bool {
